42905e8ff1810e9b494e67cded00d49cb99f2749
Zach Wily <zach@zwily.com>
2012-04-16 23:34:32 -0600

    switch to use libxml, and use libxml for canonicalization

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper.rb')
 
 require 'ruby-debug'
+require 'rexml/document'
 
 describe Onelogin::Saml::Response do
   describe "decrypting assertions" do
@@ -15,18 +16,20 @@ describe Onelogin::Saml::Response do
     
     it "should find the right attributes from an encrypted assertion" do
       @response = Onelogin::Saml::Response.new(@xmlb64, @settings)
-      REXML::XPath.first(@response.document, "/samlp:Response/saml:Assertion").should_not be_nil
-      REXML::XPath.first(@response.document, "/samlp:Response/saml:Assertion/ds:Signature/ds:SignedInfo/ds:Reference/ds:DigestValue").text.should == "eMQal6uuWKMbUMbOwBfrFH90bzE="
+      document = REXML::Document.new(@response.decrypted_document.to_s)
+      REXML::XPath.first(document, "/samlp:Response/saml:Assertion").should_not be_nil
+      REXML::XPath.first(document, "/samlp:Response/saml:Assertion/ds:Signature/ds:SignedInfo/ds:Reference/ds:DigestValue").text.should == "eMQal6uuWKMbUMbOwBfrFH90bzE="
       @response.name_qualifier.should == "http://saml.example.com:8080/opensso"
       @response.session_index.should == "s2c57ee92b5ca08e93d751987d591c58acc68d2501"
       @response.status_code.should == "urn:oasis:names:tc:SAML:2.0:status:Success"
       @response.status_message.strip.should == ""
     end
-  
+    
     it "should not be able to decrypt without the proper key" do
       @settings.xmlsec_privatekey = fixture_path("wrong-key.pem")
       @response = Onelogin::Saml::Response.new(@xmlb64, @settings)
-      REXML::XPath.first(@response.document, "/samlp:Response/saml:Assertion").should be_nil
+      document = REXML::Document.new(@response.document.to_s)
+      REXML::XPath.first(document, "/samlp:Response/saml:Assertion").should be_nil
       @response.name_qualifier.should be_nil
     end
   end
@@ -34,11 +37,11 @@ describe Onelogin::Saml::Response do
   it "should use namespaces correctly to look up attributes" do
     @xmlb64 = Base64.encode64(File.read(fixture_path("test2-response.xml")))
     @settings = Onelogin::Saml::Settings.new
-    @response= Onelogin::Saml::Response.new(@xmlb64, @settings)
+    @response = Onelogin::Saml::Response.new(@xmlb64, @settings)
     @response.name_id.should == "zach@example.com"
     @response.name_qualifier.should == "http://saml.example.com:8080/opensso"
     @response.session_index.should == "s2c57ee92b5ca08e93d751987d591c58acc68d2501"
     @response.status_code.should == "urn:oasis:names:tc:SAML:2.0:status:Success"
-    @response.status_message.should be_nil
+    @response.status_message.should == ""
   end
 end
