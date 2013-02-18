680987ce7b1702bc90ce93f28839b225710f600b
phinze <paul.t.hinze@gmail.com>
2013-02-18 09:48:26 -0600

    publish pubkey for signing as well as encryption

--- spec/meta_data_spec.rb
+++ spec/meta_data_spec.rb
@@ -26,4 +26,17 @@ describe Onelogin::Saml::MetaData do
     services[1].attributes["Location"].should == @settings.assertion_consumer_service_url[1]
   end
 
+  it "publishes the public key for both encryption and signing" do
+    settings = Onelogin::Saml::Settings.new(
+      :xmlsec_certificate => fixture_path("test1-cert.pem"),
+      :xmlsec_privatekey => fixture_path("test1-key.pem"),
+      :idp_slo_target_url => "http://idp.example.com/saml2",
+      :idp_cert_fingerprint => 'def18dbed547cdf3d52b627f41637c443045fe33'
+    )
+    doc = REXML::Document.new Onelogin::Saml::MetaData.create(settings)
+    key_descriptors = REXML::XPath.match(doc, "//KeyDescriptor")
+    key_descriptors.should have(2).keys
+    key_descriptors[0].attributes["use"].should == "encryption"
+    key_descriptors[1].attributes["use"].should == "signing"
+  end
 end
